e093e6e4238c29abf6fb7aceb31b4f54accf1344
beratukiqi <berat.u@live.se>
2023-10-11 11:20:37 +0200

    OpenAI API set up, not working yet

--- src/components/GPTModule.jsx
+++ src/components/GPTModule.jsx
@@ -1,11 +1,32 @@
 import { useContext, useEffect, useState } from "react";
 import Task from "./Task";
 import MyContext from "../main";
+// import { Configuration, OpenAIApi } from "openai";
+
 function GPTModule({ setPopupVisible }) {
+    // const configuration = new Configuration({
+    //     organization: "org-h95CoawxojtJxquEgL3PVNty",
+    //     apiKey: "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",
+    // });
+    // const openai = new OpenAIApi(configuration);
+
+    // async function GetApiResponse() {
+    //     const completion = await openai.createChatCompletion({
+    //         model: "gpt-3.5-turbo",
+    //         messages: [{ role: "user", content: "Hello there" }],
+    //     });
+
+    //     console.log(" API CALLED ", completion.data.choices[0].message);
+    // }
+
     const [inputValue, setInputValue] = useState();
     const [generatedData, setGeneratedData] = useState([]);
     const [loaded, setLoaded] = useState(false);
     const { aiData, updateAiData } = useContext(MyContext);
+
+    // useEffect(() => {
+    //     GetApiResponse();
+    // }, []);
     const generateResponse = () => {
         // Anropet GPT API
         // FÅ ETT SVAR
@@ -13,7 +34,7 @@ function GPTModule({ setPopupVisible }) {
 
         const data = [
             {
-                text: "Hej hej",
+                text: "Hej ",
             },
             {
                 text: "Hej HEJ 222",
